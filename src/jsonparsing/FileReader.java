3ec140acb6acd306d11eb63aec762edd0eb89024
Alexandre Terrasa <alexandre@moz-code.org>
2013-09-17 21:24:24 -0400

    lib: Mis à jour FileRead à partir de la version de Jacques
    voir https://github.com/jacquesberger/JSONParsingExample
    
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/jsonparsing/FileReader.java
+++ src/jsonparsing/FileReader.java
@@ -15,16 +15,14 @@ package jsonparsing;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 
 public class FileReader {
-    public static String loadFileIntoString(String filePath) throws FileNotFoundException, IOException {
-        byte[] fullSizeBuffer = new byte[(int) new File(filePath).length()];
-        FileInputStream inputStream = new FileInputStream(filePath);
-        inputStream.read(fullSizeBuffer);
-        return new String(fullSizeBuffer);
+    public static String loadFileIntoString(String filePath, String fileEncoding) throws FileNotFoundException, IOException {
+        return IOUtils.toString(new FileInputStream(filePath), fileEncoding);
     }
 }
